ff8ffa0ec697593f202d659742fbec14665b7388
James Price <jamesmpdev@gmail.com>
2021-07-13 16:26:25 +0100

    adding context to cart items

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/CartContext";
 
 const HeaderCartButton = ({ onClick }) => {
+  const context = useContext(CartContext);
+
+  const numOfCartItems = context.items.reduce((currentNumber, item) => {
+      return currentNumber + item.amount;
+  }, 0)
+
   return (
     <button onClick={onClick} className={classes.button}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Basket</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numOfCartItems}</span>
     </button>
   );
 };
