7546c1b65eb8abe474dfbc99bab3fe886d05a3cd
James Price <jamesmpdev@gmail.com>
2021-07-13 13:54:15 +0100

    adding form to mealItem

---
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from "./MealItem.module.css";
+
+const MealItem = ({ name, id, description, price }) => {
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{name}</h3>
+        <div className={classes.description}>{description}</div>
+        <div className={classes.price}>{`£${price}`}</div>
+      </div>
+      <div></div>
+    </li>
+  );
+};
+
+export default MealItem;
