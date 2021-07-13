390cab7946640f3ff913e2b556b2fddbd63f90f2
James Price <jamesmpdev@gmail.com>
2021-07-13 13:12:23 +0100

    sorting styling of header button

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import classes from "./Header.module.css";
 
 import bannerImg from "../../assets/banner.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = () => {
   return (
     <>
       <header className={classes.header}>
         <h1>Cafe-Direct</h1>
-        <button>Cart</button>
+        <HeaderCartButton>Cart</HeaderCartButton>
       </header>
       <div className={classes["main-image"]}>
         <img src={bannerImg} alt="Bags of Coffee" />
